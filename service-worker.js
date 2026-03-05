086cdda09274f5cd857093d8c9fd54625e4bd0a8
meredeitor <hperez@caborca.com.mx>
2026-03-05 15:16:11 -0600

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,30 @@
+self.addEventListener("install", e => {
+
+e.waitUntil(
+
+caches.open("auditoria-cache").then(cache => {
+
+return cache.addAll([
+"./",
+"./index.html"
+])
+
+})
+
+)
+
+})
+
+self.addEventListener("fetch", e => {
+
+e.respondWith(
+
+caches.match(e.request).then(res => {
+
+return res || fetch(e.request)
+
+})
+
+)
+
+})

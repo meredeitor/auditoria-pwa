03b4fed76a319c40a81ef51ecce6cb64c086ae33
meredeitor <hperez@caborca.com.mx>
2026-03-05 16:40:33 -0600

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,30 +1,68 @@
-self.addEventListener("install", e => {
+const CACHE_NAME = "auditoria-pwa-v3";
 
-e.waitUntil(
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./service-worker.js",
+  "./historico.html",
+  "./dashboard.html",
+  "./reporte.html",
+  "./icon-192.png",
+  "./icon-512.png"
+];
 
-caches.open("auditoria-cache").then(cache => {
+self.addEventListener("install", event => {
 
-return cache.addAll([
-"./",
-"./index.html"
-])
+  self.skipWaiting();
 
-})
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
 
-)
+});
 
-})
+self.addEventListener("activate", event => {
 
-self.addEventListener("fetch", e => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
 
-e.respondWith(
+  self.clients.claim();
 
-caches.match(e.request).then(res => {
+});
 
-return res || fetch(e.request)
+self.addEventListener("fetch", event => {
 
-})
+  event.respondWith(
 
-)
+    caches.match(event.request)
+      .then(response => {
 
-})
+        if (response) {
+          return response;
+        }
+
+        return fetch(event.request).then(networkResponse => {
+
+          return caches.open(CACHE_NAME).then(cache => {
+            cache.put(event.request, networkResponse.clone());
+            return networkResponse;
+          });
+
+        });
+
+      })
+
+  );
+
+});
